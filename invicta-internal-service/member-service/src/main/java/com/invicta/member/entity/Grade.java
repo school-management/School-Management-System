9b615247dd030b7e1f2c588781955a367b6a3e5a
puthiyajothy <saidputhi@gmail.com>
2019-10-11 11:07:49 +0530

    new changes update

--- invicta-internal-service/member-service/src/main/java/com/invicta/member/entity/Grade.java
+++ invicta-internal-service/member-service/src/main/java/com/invicta/member/entity/Grade.java
@@ -1,16 +1,11 @@
 package com.invicta.member.entity;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @SuppressWarnings("serial")
 @Entity
@@ -22,18 +17,6 @@ public class Grade implements Serializable {
 	private Long gradeId;
 	private int gradeName;
 
-//	
-//	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-//	@OneToMany(mappedBy = "grade", fetch = FetchType.EAGER)
-//	private List<Division> division;
-	
-//	public List<Division> getDivision() {
-//		return division;
-//	}
-//
-//	public void setDivision(List<Division> division) {
-//		this.division = division;
-//	}
 
 	public Long getGradeId() {
 		return gradeId;
