beb7a8c260451e94145bdcc9ca6a29646abec69f
DalistaaA <adalistaa@gmail.com>
2019-10-14 13:50:34 +0530

    for pull

--- invicta-internal-service/exam-service/src/main/java/com/invicta/exam/ExamServiceApplication.java
+++ invicta-internal-service/exam-service/src/main/java/com/invicta/exam/ExamServiceApplication.java
@@ -22,7 +22,7 @@ public class ExamServiceApplication {
 	public Docket swaggerConfiguration() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors
-				.basePackage("com.invicta.member.controller"))
+				.basePackage("com.invicta.exam.controller"))
 				.paths(PathSelectors.ant("/Api/**"))
 				.build();
 
